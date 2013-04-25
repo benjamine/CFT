3f1b7a031f9cf2a981c0c7a0ea7c3a64bb464703
Benjamin Eidelman <beneidel@gmail.com>
2013-04-25 16:01:10 -0300

    added powershell script

---
+++ CFT/content/cft.ps1
@@ -0,0 +1,77 @@
+﻿param(
+    [string]$Configuration = "",
+    [string]$Path = "~",
+    [string]$ConfigurationNameDefault = "",
+    [string]$ConfigurationNameFile = "",
+    [string]$ConfigurationNameEnv = ""
+)
+
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+Function FindPackageTool([string]$packageName, [string]$tool) {
+    $container = $scriptPath
+    
+    if (Test-Path(Join-Path $container $tool)) {
+        # tool found next to the script file
+        return Join-Path $container $tool
+    }
+    if (Test-Path(Join-Path (Join-Path $container bin) $tool)) {
+        # tool found on bin folder (useful for websites)
+        return Join-Path (Join-Path $container bin) $tool
+    }
+
+    # find in packages folder
+    while (!(Test-Path(Join-Path $container "packages"))) {
+        $containerParent = Join-Path $container ".." -resolve
+        if ($container -eq $containerParent) {
+            Throw "packages folder not found"
+        }
+        $container = $containerParent
+    }
+    $container = Join-Path $container "packages"
+    $packagePath = (Get-ChildItem -Path $container -Filter "$packageName.*" | Select-Object -First 1).FullName
+    if (!$packagePath) {
+        Throw "Package not found: $packageName"
+    }
+    $packageToolsPath = Join-Path $packagePath "tools"
+    $toolPath = Join-Path $packageToolsPath $tool -resolve
+    if (!(Test-Path $toolPath)) {
+        Throw "Tool not found. tool: $tool, package: $packageName"
+    }
+    return $toolPath
+}
+
+if ($Path.StartsWith("~")) {
+    $Path = Join-Path $scriptPath $Path.SubString(1)
+}
+
+if (!$Configuration) {
+    if ($ConfigurationNameFile -and (Test-Path(Join-Path $Path $ConfigurationNameFile))) {
+        $Configuration = Get-Content(Join-Path $Path $ConfigurationNameFile)
+        if ($Configuration) {
+            Write-Host "Using configuration $Configuration obtained from $ConfigurationNameFile"
+        }
+    }
+    if (!$Configuration -and $ConfigurationNameEnv) {
+        $configuration = [environment]::GetEnvironmentVariable($ConfigurationNameEnv)
+        if ($Configuration) {
+            Write-Host "Using configuration $Configuration obtained from env:$ConfigurationNameEnv"
+        }
+    }
+    if (!$Configuration) {
+        $Configuration = $ConfigurationNameDefault
+        if ($Configuration) {
+            Write-Host "Using default configuration $Configuration"
+        }
+    }
+    if (!$Configuration) {
+        Write-Host "No configuration name specified"
+    }
+} else {
+    Write-Host "Using configuration $Configuration"
+}
+
+$cftPath = FindPackageTool "cft" "cft.exe"
+
+$command = ((FindPackageTool "cft" "cft.exe") + " $Path $Configuration")
+Invoke-Expression $command
--- README.md
+++ README.md
@@ -31,3 +31,24 @@ These tokens will be replaced at the end of transformation:
 ## Nuget
 
 Install as a nuget package: https://nuget.org/packages/cft/
+
+cft.ps1 will be added to your project, in order to distribute it with your app Include it on the project and Copy to Output.
+
+This script can be added to your prebuild event and/or run on your post-deploy tasks. It will find cft.exe on /packages folder or locally and allows you to obtain the configuration name from different sources.
+
+``` powershell
+    # transform config files in the project using Production configuration name
+    .\cft.ps1 Production
+
+    # transform config files in all projects
+    .\cft.ps1 Production -Path ~\..
+
+    # transform config files using configuration name obtained from a file
+    .\cft.ps1 -ConfigurationNameFile configurationName.tmp
+
+    # transform config files using configuration name obtained from an env variable
+    .\cft.ps1 -ConfigurationNameEnv CONFIGURATIONNAME
+
+    # transform config files with fallback thru different sources (file, env, default)
+    .\cft.ps1 -ConfigurationNameFile configurationName.tmp -ConfigurationNameEnv CONFIGURATIONNAME -ConfigurationNameDefault LocalW
+```
